d4d569e139531e7c18dc1f8adf1f72af1b953dd8
Son Trinh <son.trinh@employmenthero.com>
2018-12-01 10:42:26 +0700

    introduce register form

---
+++ client/src/components/Register/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import compose from 'recompose/compose';
+
+import RegisterForm from './RegisterForm';
+
+const FORM_NAME = 'EcBooking/RegisterForm';
+
+const Register = () => (
+  <div className="auth-form-wrapper">
+    <div className="container">
+      <RegisterForm />
+    </div>
+  </div>
+);
+
+const withReduxForm = reduxForm({ form: FORM_NAME });
+
+export default withReduxForm(Register);
