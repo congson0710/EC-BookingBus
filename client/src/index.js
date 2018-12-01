d4ecab7db4cda7bedf3e72eeac0d2e41557f085a
Son Trinh <son.trinh@employmenthero.com>
2018-12-01 11:14:16 +0700

    fix conflict

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-<<<<<<< HEAD
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-=======
-import 'bootstrap/dist/css/bootstrap.min.css'
->>>>>>> master
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 import { store } from './redux';
