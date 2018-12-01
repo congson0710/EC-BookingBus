4b06bc9e2ca6d56bb6b39a4691cec1d4292fa7bb
Son Trinh <son.trinh@employmenthero.com>
2018-12-01 21:09:11 +0700

    register reducer

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,9 +1,49 @@
-// export const userReducer = (state = {}, action) => {
-//   switch (action.type) {
-//     case 'test': {
-//       break;
-//     }
-//     default:
-//       break;
-//   }
-// }
+import get from 'lodash/fp/get';
+
+import {
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL
+} from '../actions/actionsTypes';
+
+const initialState = {
+  userRegister: {
+    userData: {},
+    isLoading: false,
+    error: {}
+  }
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST: {
+      return {
+        ...state,
+        userRegister: {
+          ...state.userRegister,
+          isLoading: true
+        }
+      };
+    }
+    case USER_REGISTER_SUCCESS: {
+      return {
+        ...state,
+        userRegister: {
+          ...state.userRegister,
+          userData: get('payload')(action)
+        }
+      };
+    }
+    case USER_REGISTER_FAIL: {
+      return {
+        ...state,
+        userRegister: {
+          ...state.userRegister,
+          error: get('payload')(action)
+        }
+      };
+    }
+    default:
+      break;
+  }
+};
