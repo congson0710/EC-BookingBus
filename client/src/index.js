ec1a256f7b244b9241793a463463ec56c0986923
Son Trinh <son.trinh@employmenthero.com>
2018-12-01 10:41:20 +0700

    apply store

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App'
-// import { store } from './redux'
-import * as serviceWorker from './serviceWorker'
+import App from './App';
+import { store } from './redux';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  // <Provider>
-    <App />,
-  // </Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
