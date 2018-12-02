a8dc9ae5e474f9dea4d446f7b6d050dad6a29bcc
Son Trinh <son.trinh@employmenthero.com>
2018-12-02 18:27:27 +0700

    handle login post route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,8 +2,13 @@ import get from 'lodash/fp/get';
 
 module.exports = app => {
   app.post('/api/register', (req, res) => {
-    const registerData = req.body;
+    const registerData = get('body')(req);
     console.log('regis data', registerData);
-    res.send('kdjfnvjkdfnjkv');
+    res.status(200).send('success');
+  });
+  app.post('/api/login', (req, res) => {
+    const loginData = get('body')(req);
+    console.log('login data', loginData);
+    res.status(200).send('success');
   });
 };
