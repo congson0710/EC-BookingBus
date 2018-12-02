03612dd0ee26952b083eb31db8c36f17961e4407
Son Trinh <son.trinh@employmenthero.com>
2018-12-02 18:24:13 +0700

    handle user login

--- client/src/redux/actions/userAction.js
+++ client/src/redux/actions/userAction.js
@@ -2,7 +2,10 @@ import axios from '../../axios';
 import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
-  USER_REGISTER_FAIL
+  USER_REGISTER_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL
 } from './actionsTypes';
 
 export const registerThunkCreator = registerData => async dispatch => {
@@ -21,6 +24,28 @@ export const registerThunkCreator = registerData => async dispatch => {
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload: error
+    });
+    console.error(error);
+  }
+};
+
+export const loginThunkCreator = loginData => async dispatch => {
+  dispatch({
+    type: USER_LOGIN_REQUEST
+  });
+
+  try {
+    const response = await axios.post('/api/login', loginData);
+    if (response) {
+      dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: response.data
+      });
+    }
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
       payload: error
     });
     console.error(error);
