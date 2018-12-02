3f3a0719968f0c1a8311f674c4b8e379509c7bf5
Son Trinh <son.trinh@employmenthero.com>
2018-12-02 16:54:01 +0700

    config passport js

---
+++ server/services/passportJs/index.js
@@ -0,0 +1,32 @@
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import { userModel as Users } from '../sequelize/models/userModel';
+
+passport.serializeUser((user, done) => {
+  done(null, user.userID);
+});
+
+passport.deserializeUser((user, done) => {
+  Users.findById(user.userID).then(currentUser => {
+    done(null, currentUser);
+  });
+});
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'userName',
+      passwordField: 'userPassword'
+    },
+    async (username, password, done) => {
+      const currentUser = await Users.findOne({
+        where: { userName: username, userPassword: password }
+      });
+      if (currentUser) {
+        return done(null, currentUser);
+      } else {
+        return done(null, false, { message: 'Invalid username or password.' });
+      }
+    }
+  )
+);
