aa1292f304e013b6b49cc411fa54d1236c9eabdc
Son Trinh <son.trinh@employmenthero.com>
2018-12-02 10:34:58 +0700

    prepare props

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
+import flow from 'lodash/fp/flow';
 import compose from 'recompose/compose';
+import defaultProps from 'recompose/defaultProps';
+import mapProps from 'recompose/mapProps';
 
 import RegisterForm from './RegisterForm';
+import { registerThunkCreator } from '../../redux/actions/userAction';
 
 const FORM_NAME = 'EcBooking/RegisterForm';
 
@@ -15,28 +19,38 @@ const Register = ({ handleSubmit }) => (
   </div>
 );
 
-const onSubmit = data => {
-  console.log('data to submit', data);
-};
-
-const initialRoleValue = {
-  userRole: 1
-};
-
-const connectToReduxStore = connect(
+const connectToRedux = connect(
   null,
-  null
+  dispatch => ({
+    onRequestRegister: flow(
+      registerThunkCreator,
+      dispatch
+    )
+  })
 );
 
+const prepareProps = mapProps(({ onRequestRegister, ...otherProps }) => ({
+  onSubmit: data => {
+    onRequestRegister(data);
+  },
+  ...otherProps
+}));
+
+const withConfig = defaultProps({
+  initialValues: {
+    userRole: 1
+  }
+});
+
 const withReduxForm = reduxForm({
-  form: FORM_NAME,
-  initialValues: initialRoleValue,
-  onSubmit
+  form: FORM_NAME
 });
 
 const enhance = compose(
-  withReduxForm,
-  connectToReduxStore
+  connectToRedux,
+  prepareProps,
+  withConfig,
+  withReduxForm
 );
 
 export default enhance(Register);
