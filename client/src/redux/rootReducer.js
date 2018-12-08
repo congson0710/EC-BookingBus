61619e5cab9f21ff6933321b9da9282e177a94ef
Son Trinh <son.trinh@employmenthero.com>
2018-12-08 15:58:03 +0700

    define search reducer

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -2,14 +2,23 @@ import { combineReducers } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 
 import { userReducer } from './reducers/userReducer';
-import {
+import bookingReducerPaths, {
   searchTicketReducer,
   fetchListTicketReducer
 } from './reducers/bookingReducer';
+import searchingReducerPaths, {
+  fetchListPlaceReducer
+} from './reducers/searchingReducer';
+import {
+  FETCH_LIST_PLACE,
+  FETCH_LIST_TICKET,
+  SEARCH_TICKET
+} from './actions/actionsTypes';
 
 export const rootReducer = combineReducers({
   form: reduxFormReducer,
   userReducer,
-  searchTicketReducer,
-  fetchListTicketReducer
+  [bookingReducerPaths[SEARCH_TICKET]]: searchTicketReducer,
+  [bookingReducerPaths[FETCH_LIST_TICKET]]: fetchListTicketReducer,
+  [searchingReducerPaths[FETCH_LIST_PLACE]]: fetchListPlaceReducer
 });
