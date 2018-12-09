16f0b69ae21d76129d4810d8d7df3aeb95f94fbc
Tien Tran <tientm@dgroup.co>
2018-12-09 16:52:40 +0700

    add authorize localstorage

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { JWT } from '../config';
 
 export function getIdFromToken(req, res, next) {
-  const token = req.headers.authorization.split(' ')[1];
-  req.userID = jwt.verify(token, JWT.secret);
+  const token = req.headers.authorization ? req.headers.authorization.split(' ')[1]: '';
+  req.userID = token ? jwt.verify(token, JWT.secret) : '';
   next();
 }
