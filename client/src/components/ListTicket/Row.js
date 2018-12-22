e722f9782cc073f387507bfc80e73327af97a34c
Son Trinh <son.trinh@employmenthero.com>
2018-12-22 14:48:04 +0700

    handle search ticket

--- client/src/components/ListTicket/Row.js
+++ client/src/components/ListTicket/Row.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import eq from 'lodash/fp/eq';
+import get from 'lodash/fp/get';
+import flow from 'lodash/fp/flow';
 
-const PureRow = ({
-  row: { busCompanyName, startDay, endDay, ticketKind, price }
-}) => (
+const TICKET_STATUS = 'SOLD';
+const isTicketSold = eq(TICKET_STATUS);
+const getBusCompanyName = busRoute =>
+  flow(
+    get('bus'),
+    get('bus_company'),
+    get('companyName')
+  )(busRoute);
+
+const getStartPlace = busRoute =>
+  flow(
+    get('startPlace'),
+    get('placeName')
+  )(busRoute);
+const getEndPlace = busRoute =>
+  flow(
+    get('endPlace'),
+    get('placeName')
+  )(busRoute);
+
+const PureRow = ({ row: { ticketID, bus_route, status, price } }) => (
   <tr>
-    <td>{busCompanyName}</td>
-    <td>{startDay}</td>
-    <td>{endDay}</td>
-    <td>{ticketKind}</td>
+    <td>{getBusCompanyName(bus_route)}</td>
+    <td>{getStartPlace(bus_route)}</td>
+    <td>{getEndPlace(bus_route)}</td>
+    <td>{bus_route.seatNumber}</td>
     <td>{price}</td>
+    <td>{isTicketSold(status) ? 'Hết vé' : 'Còn vé'}</td>
     <td>
-      <button className="btn btn-danger">Mua vé</button>
+      {!isTicketSold(status) ? (
+        <button className="btn btn-danger">Mua vé</button>
+      ) : null}
     </td>
   </tr>
 );
--- client/src/redux/selectors/searchingSelectors.js
+++ client/src/redux/selectors/searchingSelectors.js
@@ -1,4 +1,4 @@
-import { rawDataSelectorCreator } from '../share';
+import { rawSelectorCreator } from '../share';
 
 import {
   START_LIST_PLACE,
@@ -7,22 +7,27 @@ import {
   SEARCH_TICKET
 } from '../reducers/_reducerSectionNames';
 
-export const fetchStartListPlaceDataSelector = rawDataSelectorCreator(
+export const fetchStartListPlaceDataSelector = rawSelectorCreator(
   'SEARCH_REDUCER',
   START_LIST_PLACE
-);
+)('data');
 
-export const fetchEndListPlaceDataSelector = rawDataSelectorCreator(
+export const fetchEndListPlaceDataSelector = rawSelectorCreator(
   'SEARCH_REDUCER',
   END_LIST_PLACE
-);
+)('data');
 
-export const reFetchListPlaceDataSelector = rawDataSelectorCreator(
+export const reFetchListPlaceDataSelector = rawSelectorCreator(
   'SEARCH_REDUCER',
   FILTER_LIST_PLACE
-);
+)('data');
 
-export const searchTicketDataSelector = rawDataSelectorCreator(
+export const searchTicketDataSelector = rawSelectorCreator(
   'SEARCH_REDUCER',
   SEARCH_TICKET
-);
+)('data');
+
+export const isSearchingTicketSelector = rawSelectorCreator(
+  'SEARCH_REDUCER',
+  SEARCH_TICKET
+)('isLoading');
