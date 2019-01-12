1dfee65c2ad1088f185474fee5ab9adea2c63852
TIen Tran <tientranmac96@gmail.com>
2019-01-12 16:11:25 +0700

    ready to deploy

---
+++ client/build/precache-manifest.3779e6ea123c9e4b96d2912b46df175e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "06664ae1959a58f8f90b0839fcd09961",
+    "url": "/static/media/home.06664ae1.jpg"
+  },
+  {
+    "revision": "c155eccc0459c4553101",
+    "url": "/static/css/main.778072a4.chunk.css"
+  },
+  {
+    "revision": "326fdb520f223b987fdc462d649c58f1",
+    "url": "/static/media/top_4.326fdb52.jpg"
+  },
+  {
+    "revision": "cac297f62a1ea42d6368",
+    "url": "/static/js/1.cac297f6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c155eccc0459c4553101",
+    "url": "/static/js/main.c155eccc.chunk.js"
+  },
+  {
+    "revision": "3bcce370ba137b90407c0b9709b833a3",
+    "url": "/static/media/top_1.3bcce370.jpg"
+  },
+  {
+    "revision": "27282a973dafea22ed6bbf9428b935a9",
+    "url": "/static/media/top_2.27282a97.jpg"
+  },
+  {
+    "revision": "bb60bc4e56ee67874c41f427c8d349cd",
+    "url": "/static/media/top_3.bb60bc4e.jpg"
+  },
+  {
+    "revision": "cac297f62a1ea42d6368",
+    "url": "/static/css/1.13333879.chunk.css"
+  },
+  {
+    "revision": "2aa48b474be82d60042787d344cd0c2f",
+    "url": "/index.html"
+  }
+];
--- client/src/axios/index.js
+++ client/src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({ baseURL: 'http://localhost:5000' });
+const axiosInstance = axios.create({ baseURL: '' });
 
 export function setToken(token) {
   return axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
